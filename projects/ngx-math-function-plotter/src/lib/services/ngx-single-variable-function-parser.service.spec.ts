bcf40f5ea224fbbf427a9828fa5904cdb7b0d181
AliEzzatOdeh <aliezzat1993@outlook.com>
2019-10-13 11:37:46 +0300

    Added more unit tests for NgxSingleVariableParser

--- projects/ngx-math-function-plotter/src/lib/services/ngx-single-variable-function-parser.service.spec.ts
+++ projects/ngx-math-function-plotter/src/lib/services/ngx-single-variable-function-parser.service.spec.ts
@@ -15,4 +15,28 @@ describe('NgxSingleVariableFunctionParserService', () => {
     service.setMathFunctionText('(1+2)*(8/4)');
     expect(service.computeFunctionAtValue(0)).toBe(6);
   });
+
+  it('should resolve expression (x^2)*(x-1) at x=4 to be 48', () => {
+    const service: NgxSingleVariableFunctionParserService = TestBed.get(NgxSingleVariableFunctionParserService);
+    service.setMathFunctionText('(x^2)*(xsub1)');
+    expect(service.computeFunctionAtValue(4)).toBe(48);
+  });
+
+  it('should resolve expression sqr(x) at x=16 to be 4', () => {
+    const service: NgxSingleVariableFunctionParserService = TestBed.get(NgxSingleVariableFunctionParserService);
+    service.setMathFunctionText('sqr(x)');
+    expect(service.computeFunctionAtValue(16)).toBe(4);
+  });
+
+  it('should resolve expression sin(x)-cos(x) at x=0 to be -1', () => {
+    const service: NgxSingleVariableFunctionParserService = TestBed.get(NgxSingleVariableFunctionParserService);
+    service.setMathFunctionText('sin(x)subcos(x)');
+    expect(service.computeFunctionAtValue(0)).toBe(-1);
+  });
+
+  it('should resolve expression tan(x)+cos(x) at x=0 to be 1', () => {
+    const service: NgxSingleVariableFunctionParserService = TestBed.get(NgxSingleVariableFunctionParserService);
+    service.setMathFunctionText('tan(x)+cos(x)');
+    expect(service.computeFunctionAtValue(0)).toBe(1);
+  });
 });
